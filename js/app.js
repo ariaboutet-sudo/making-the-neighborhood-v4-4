31266f84f5508c274080d94bb817c29dd7c03d24
ariaboutet-sudo <aria.boutet@gmail.com>
2026-07-26 22:17:13 -0400

    Update app.js
    Remove red overlay test

--- js/app.js
+++ js/app.js
@@ -28,8 +28,8 @@ Object.assign(themeLens.style, {
   inset: "0",
   zIndex: "999",
   pointerEvents: "none",
-  opacity: ".6",
-  backgroundColor: "red",
+  opacity: "0",
+  backgroundColor: "transparent",
   transition: "opacity 0.45s ease, background-color 0.45s ease"
 });
 map.getContainer().appendChild(themeLens);
