5d30b222d4edd5286fc2963f65ecf89cd6b7a218
ariaboutet-sudo <aria.boutet@gmail.com>
2026-08-07 23:32:34 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -87,16 +87,6 @@ function selectTheme(key) {
   document.getElementById("site-detail").innerHTML = "";
   activeTheme = activeTheme === key ? null : key;
 
-  if (activeTheme) {
-  if (map.hasLayer(parkImageOverlay)) {
-    map.removeLayer(parkImageOverlay);
-  }
-} else {
-  if (!map.hasLayer(parkImageOverlay)) {
-    parkImageOverlay.addTo(map);
-  }
-}
-
   renderButtons();
   renderThemeCopy();
   renderMarkers();
